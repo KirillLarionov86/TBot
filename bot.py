a0fff56096c660f038f717c990503afdefe71047
KirillLarionov86 <W9779745@fmail.com>
2024-08-20 00:11:34 +0300

    Add missing import for CommandHandler in bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
-from telegram.ext import Application
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from telegram import Update
 from telegram.ext import ContextTypes
 import os
